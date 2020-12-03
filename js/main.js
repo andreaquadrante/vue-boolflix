bfff2b30bcf9720a9142f8e446262002fc484d57
Andrea <quadranteandrea@gmail.com>
2020-12-03 15:35:05 +0100

    adds axios parts

--- js/main.js
+++ js/main.js
@@ -1,12 +1,27 @@
+const API_KEY = "0cbad98882962931e244c2cc93535043"
+
 const app = new Vue ({
 
-  el: "#root",
+  el: '#root',
 
   data: {
-
+    movies: [],
+    searchInput: ''
   },
 
   mounted: function () {
+
+    axios
+      .get ("https://api.themoviedb.org/3/search/movie?", {
+        params: {
+          'api_key': API_KEY,
+          query: this.searchInput
+        }
+      })
+      .then (got => {
+        this.movies = got.data;
+        this.searchMovie = '';
+      })
 
   },
 
