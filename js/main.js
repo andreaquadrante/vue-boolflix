5f9a99b2c654b38b0ff8869e41b687426ea78a5f
Andrea <quadranteandrea@gmail.com>
2020-12-09 04:54:47 +0100

    milestone 4
    fixes needed

--- js/main.js
+++ js/main.js
@@ -1,28 +1,52 @@
-const API_KEY = "0cbad98882962931e244c2cc93535043"
+const API_KEY = '0cbad98882962931e244c2cc93535043';
 
 const app = new Vue ({
 
   el: '#root',
 
   data: {
-    movies: [],
-    searchInput: ''
+
+    apiKey: API_KEY,
+    searchMovie: [],
+    searchInput: '',
+    rating: 5,
+    posterUri: 'https://image.tmdb.org/t/p/w342/',
+    currentPage: 1,
+    totalPages: 1
+
   },
 
-  mounted: function () {
+  methods: {
+
+    search () {
+      axios
+        .get ('https://api.themoviedb.org/3/search/multi', {
+          params: {'api_key': this.apiKey,
+          query: this.searchInput,
+          page: this.currentPage,
+        }})
+        .then ((got) => {this.searchMovie = got.data.results;
+          this.totalPages = got.data.total_pages;
+        });
+    },
+
+    stars (rate) {
+      return Math.ceil (rate / 2)
+    },
+
+  },
+
+  mounted () {
 
     axios
-      .get ("https://api.themoviedb.org/3/search/movie?", {
-        params: {
-          'api_key': API_KEY,
-          query: this.searchInput
-        }
-      })
-      .then (got => {
-        this.movies = got.data;
-        this.searchMovie = '';
+      .get ('https://api.themoviedb.org/3/movie/popular', {
+        params: {'api_key': this.apiKey,
+        page: this.currentPage,
+      }})
+      .then (got => {this.searchMovie = got.data.results;
+        this.totalPages = got.data.total_pages;
       })
 
-  },
+  }
 
-});
+})
